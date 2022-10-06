0355f9abf51cedd9cce21413cdf2d32e75d8f431
thanos <chaddwick25@yahoo.ca>
2022-10-06 18:01:24 -0400

    Added the ConfigService to avalanche module

--- src/avalanche/avalanche.module.ts
+++ src/avalanche/avalanche.module.ts
@@ -3,11 +3,12 @@ import { AvalancheService } from './avalanche.service';
 import { AvalancheController } from './avalanche.controller';
 import { Token } from '@entities/token.entities';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Token])],
   controllers: [AvalancheController],
-  providers: [AvalancheService],
+  providers: [AvalancheService, ConfigService],
   exports: [AvalancheService]
 })
 export class AvalancheModule {}
